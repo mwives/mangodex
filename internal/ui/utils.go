ceddc50400bd5a589ebfe047508e69a06b9bbf1d
Ives M. W <ivesmw@gmail.com>
2024-10-29 01:10:43 -0300

    feat: add languageToFlag util

---
+++ internal/ui/utils.go
@@ -0,0 +1,35 @@
+package ui
+
+import "fmt"
+
+func languageToFlag(languageCode string) string {
+	fmt.Println(languageCode)
+	langFlags := map[string]string{
+		"ar":    "🇸🇦",
+		"de":    "🇩🇪",
+		"el":    "🇬🇷",
+		"en":    "🇺🇸",
+		"es-la": "🇪🇸",
+		"es":    "🇪🇸",
+		"fa":    "🇮🇷",
+		"fr":    "🇫🇷",
+		"hu":    "🇭🇺",
+		"id":    "🇮🇩",
+		"it":    "🇮🇹",
+		"ja":    "🇯🇵",
+		"pl":    "🇵🇱",
+		"pt-br": "🇧🇷",
+		"ro":    "🇷🇴",
+		"ru":    "🇷🇺",
+		"tr":    "🇹🇷",
+		"uk":    "🇬🇧",
+		"zh-hk": "🇭🇰",
+	}
+
+	flag, ok := langFlags[languageCode]
+	if !ok {
+		return languageCode
+	}
+
+	return flag
+}
